57efe79f7a411ef91b897e218a2df97c6cf97e66
Asiri Sandaruwan <1997asiri12@gmail.com>
2024-02-24 01:04:15 +0530

    Update and delete are done

--- index.js
+++ index.js
@@ -35,4 +35,34 @@ app.post("/add-product", async (req, resp) =>{
     resp.send(result);
 })
 
+app.get("/products", async (req,resp)=>{
+    const products =await Product.find();
+    if(products.length > 0){
+        resp.send(products)
+    }else{
+        resp.send({result:"No products found"})
+    }
+})
+
+app.delete("/product/:id", async (req,resp)=>{
+    let result = await Product.deleteOne({_id:req.params.id});
+    resp.send(result);
+})
+
+app.get("/product/:id", async (req,resp)=>{
+    let result = await Product.findOne({_id:req.params.id});
+    if(result){
+        resp.send(result);
+    }else{
+        resp.send({result:"No product found"})
+    }
+})
+
+app.put("/product/:id", async (req,resp)=>{
+    let result = await Product.updateOne(
+        { _id:req.params.id },
+        { $set:req.body }
+    )
+    resp.send(result);
+})
 app.listen(5000)
